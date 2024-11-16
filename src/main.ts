d4bc51eab2d79c1b6a8af5eb1393a85c0156ad7b
suichanwa <augustinnegura746@gmail.com>
2024-11-16 22:25:48 +0200

    add swager

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+/* eslint-disable prettier/prettier */
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle("Teacher Schedule API")
+    .setDescription("API documentation for the Teacher Schedule system")
+    .setVersion("1.0")
+    .addTag("schedules")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api-docs", app, document);
+
+  await app.listen(3000);
+  console.log("Application is running on: http://localhost:3000/api-docs");
 }
 bootstrap();
